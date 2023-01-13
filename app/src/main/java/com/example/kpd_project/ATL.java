24949b04c7268914a18a2769861c0f8d9ac8bceb
Shift2Ace <hei20030719@gmail.com>
2023-01-13 12:40:18 +0800

    123

---
+++ app/src/main/java/com/example/kpd_project/ATL.java
@@ -0,0 +1,20 @@
+package com.example.kpd_project;
+
+public class ATL {
+    String targetIP;
+    int udpPort;
+    int locationX;
+    int locationY;
+
+    public void commandSend(String cmd){
+
+    }
+
+    public void connectWiFi(String wifiSSID, String wifiPassword){
+
+    }
+
+    public void locate(){
+
+    }
+}
