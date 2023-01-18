d15185c3a0dfd3ad5374fd5cdcaff14daa551ada
josh112311 <pangky110@gmail.com>
2023-01-18 16:44:06 +0800

    123

--- app/src/main/java/com/example/kpd_project/FirstFragment.java
+++ app/src/main/java/com/example/kpd_project/FirstFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
